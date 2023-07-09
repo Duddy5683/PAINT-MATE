2c3acc1d85f9e795b6b11e14f81779d32e7565e2
Duddy5683 <126365878+Duddy5683@users.noreply.github.com>
2023-07-09 16:51:30 +0000

    changed background, temp

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,7 +15,7 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.5,
+    temperature: 0.4,
     max_tokens: 500,
   });
   
